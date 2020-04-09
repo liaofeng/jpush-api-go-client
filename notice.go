b3562ac5e767b57b13339a47ebb77c0bce0bd4ed
liaofeng <liaofeng@maka.im>
2020-04-09 21:35:05 +0800

    android: 支持厂商通道

--- notice.go
+++ notice.go
@@ -8,10 +8,12 @@ type Notice struct {
 }
 
 type AndroidNotice struct {
-	Alert     string                 `json:"alert"`
-	Title     string                 `json:"title,omitempty"`
-	BuilderId int                    `json:"builder_id,omitempty"`
-	Extras    map[string]interface{} `json:"extras,omitempty"`
+	Alert       string                 `json:"alert"`
+	Title       string                 `json:"title,omitempty"`
+	BuilderId   int                    `json:"builder_id,omitempty"`
+	UriActivity string                 `json:"uri_activity,omitempty"`
+	UriAction   string                 `json:"uri_action,omitempty"`
+	Extras      map[string]interface{} `json:"extras,omitempty"`
 }
 
 type IOSNotice struct {
